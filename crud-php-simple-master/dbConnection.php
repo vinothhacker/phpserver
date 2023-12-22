91570dc2add72d3b4d25054c03b9c8313b85486a
Vinoth Kumar V <70474226+vinothhacker@users.noreply.github.com>
2023-12-22 10:52:25 +0530

    Delete crud-php-simple-master directory

--- crud-php-simple-master/dbConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseHost = 'database-2.cd00ioqg2b8z.ap-south-1.rds.amazonaws.com';
-$databaseName = 'basic';
-$databaseUsername = 'admin';
-$databasePassword = '12345678';
-
-// Open a new connection to the MySQL server
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
